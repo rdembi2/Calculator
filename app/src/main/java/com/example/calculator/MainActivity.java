b0eb133d57ee2da31f496c274851875496d6d611
rdembi2 <rdembi2@illinois.edu>
2019-06-21 12:29:10 -0700

    The first round of code is complete

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -23,14 +23,18 @@ public class MainActivity extends AppCompatActivity {
     private Button sub;
     private Button mul;
     private Button div;
+    private Button equal;
+    private Button clear;
     private TextView info;
     private TextView result;
     private final char ADDITION = '+';
     private final char SUBTRACTION = '-';
     private final char MULTIPLICATION = '*';
-    private final char DIVISION = '-';
+    private final char DIVISION = '/';
+    private final char EQU = 0;
     private double val1 = Double.NaN;
     private double val2;
+    private char ACTION;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -109,6 +113,65 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        add.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                compute();
+                ACTION = ADDITION;
+                result.setText(String.valueOf(val1) + "+");
+                info.setText(null);
+            }
+        });
+
+        sub.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                compute();
+                ACTION = SUBTRACTION;
+                result.setText(String.valueOf(val1) + "-");
+                info.setText(null);
+            }
+        });
+
+        mul.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                compute();
+                ACTION = MULTIPLICATION;
+                result.setText(String.valueOf(val1) + "*");
+                info.setText(null);
+            }
+        });
+
+        div.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                compute();
+                ACTION = DIVISION;
+                result.setText(String.valueOf(val1) + "/");
+                info.setText(null);
+            }
+        });
+
+        equal.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                compute();
+                ACTION = EQU;
+                result.setText(result.getText().toString() + String.valueOf(val2) + "=" + String.valueOf(val1));
+                info.setText(null);
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(info.getText().length() > 0) {
+                    CharSequence name = info.getText().toString();
+                    info.setText(name.subSequence(0, name.length() - 1));
+                } else {
+                    val1 = Double.NaN;
+                    val2 = Double.NaN;
+                    info.setText(null);
+                    result.setText(null);
+                }
+
+            }
+        });
     }
 
     private void setupUIViews() {
@@ -126,7 +189,34 @@ public class MainActivity extends AppCompatActivity {
         sub = (Button)findViewById(R.id.btnSub);
         mul = (Button)findViewById(R.id.btnMul);
         div = (Button)findViewById(R.id.btnDiv);
+        equal = (Button)findViewById(R.id.btnEqual);
         info = (TextView)findViewById(R.id.tvControl);
         result = (TextView)findViewById(R.id.tvResult);
+        clear = (Button)findViewById(R.id.btnClear);
+    }
+
+    private void compute() {
+        if(!Double.isNaN(val1)) {
+            val2 = Double.parseDouble(info.getText().toString());
+
+            switch(ACTION) {
+                case ADDITION:
+                    val1 = val1 + val2;
+                    break;
+                case SUBTRACTION:
+                    val1 = val1 - val2;
+                    break;
+                case MULTIPLICATION:
+                    val1 = val1 * val2;
+                    break;
+                case DIVISION:
+                    val1 = val1 * val2;
+                    break;
+                case EQU:
+                    break;
+            }
+        } else {
+            val1 =  Double.parseDouble(info.getText().toString());
+        }
     }
 }
